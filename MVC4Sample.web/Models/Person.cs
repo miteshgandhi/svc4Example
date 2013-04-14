8b8ccc4c20e1fd1a5e74576148127b6833f82ba2
unknown <mitesh.gandhi27@hotmail.com>
2013-04-14 13:06:00 -0400

    Initial commit

---
+++ MVC4Sample.web/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC4Sample.web.Models
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
